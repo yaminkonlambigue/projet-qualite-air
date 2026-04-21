6454dc24a9b5345a6f871dc451398f41aef4d3e3
Horace24 <horacebodjona1@gmail.com>
2026-04-21 13:08:11 +0000

    Implémentation des modèles: CART, forêt aléatoire, régression logistique

--- src/models.py
+++ src/models.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix, f1_score
-
-#Création de la variable cible
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 
 def prepare_features(df, features, target, date_col):
@@ -23,3 +24,63 @@ def temporal_split(X, y, train_size=0.8):
     y_train = y.iloc[:split]
     y_test = y.iloc[split:]
     return X_train, X_test, y_train, y_test
+
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    results = {
+        "f1_score": f1_score(y_test, y_pred),
+        "confusion_matrix": confusion_matrix(y_test, y_pred),
+        "classification_report": classification_report(y_test, y_pred)
+    }
+    return results
+
+
+# Le modèle CART
+def train_cart(X_train, y_train, max_depth=5, min_samples_leaf=50, random_state=42):
+    model = DecisionTreeClassifier(
+        max_depth=max_depth,
+        min_samples_leaf=min_samples_leaf,
+        class_weight="balanced",
+        random_state=random_state
+    )
+    model.fit(X_train, y_train)
+    return model
+
+
+# La forêt aléatoire
+def train_random_forest(
+    X_train,
+    y_train,
+    n_estimators=100,
+    max_depth=5,
+    min_samples_leaf=50,
+    random_state=42
+):
+    model = RandomForestClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        min_samples_leaf=min_samples_leaf,
+        class_weight="balanced",
+        random_state=random_state,
+        n_jobs=-1
+    )
+    model.fit(X_train, y_train)
+    return model
+
+
+# Le modèle de regression logistique
+def train_logistic_regression(X_train, y_train, X_test):
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    model = LogisticRegression(
+        class_weight="balanced",
+        max_iter=1000
+    )
+    model.fit(X_train_scaled, y_train)
+
+    return model, scaler, X_train_scaled, X_test_scaled
+
+
